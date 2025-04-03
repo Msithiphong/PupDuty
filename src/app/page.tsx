5bdb468e2afda854e1b98b03bc2e29064735b55a
Msithiphong <Csithiphonggg@gmail.com>
2025-04-03 06:55:45 -0700

    v1 image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,7 +77,7 @@ export default function LandingPage() {
               <div className="flex items-center justify-center">
                 <div className="relative h-[350px] w-[350px] md:h-[450px] md:w-[450px]">
                   <Image
-                    src="/koda.jpg?height=450&width=450"
+                    src="/koda.jpg"
                     alt="Dog with happy owners using PupDuty app"
                     fill
                     className="object-cover rounded-lg"
